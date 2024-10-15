e460e16e040a8444722e835a552ec3314e3e8ba5
blueland <dbgus3577@gmail.com>
2024-10-15 21:10:47 +0900

    Initial Commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by blueland on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
